60311deeecb40fd5aca6c0e7d1da6a5fb1fe4155
Brandon Wade <brandon_wade2005@hotmail.com>
2018-07-23 20:24:55 -0500

    Add IsEmpty function for a File.

--- file.go
+++ file.go
@@ -8,3 +8,8 @@ type File struct {
 	Size int64  `json:"size"`
 	File os.FileInfo
 }
+
+// IsEmpty - returns a bool indicating whether a File is empty
+func (f *File) IsEmpty() bool {
+	return f.Path == "" && f.Size == 0
+}
